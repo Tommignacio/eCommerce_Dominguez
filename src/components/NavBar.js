e389bf9e63275de01eba3075c521f94d6db3743f
Tommignacio <88063640+Tommignacio@users.noreply.github.com>
2021-10-29 23:09:25 -0300

    Nuevo-Nav

---
+++ src/components/NavBar.js
@@ -0,0 +1,32 @@
+const Navbar=()=>{
+    return(
+        <>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="#">Gentleman</a>
+                <button className="navbar-toggler" type="button" datatoggle="collapse" dataarget="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Clothes</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link disabled" href="#">Buy</a>
+                    </li>
+                    </ul>
+                    <form className="form-inline my-2 my-lg-0">
+                    <input className="form-control mr-sm-2" type="search" placeholder="Search"/>
+                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
