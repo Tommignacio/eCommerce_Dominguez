021b6d6f44700850ef588f5fae342ef3dd2269e6
Tommignacio <88063640+Tommignacio@users.noreply.github.com>
2021-11-03 20:31:30 -0300

    Agregue un titulo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 import NavBar from './components/NavBar/NavBar';
+import TitleContainer from './components/TitleContainer/TitleContainer';
 
 
 function App() {
   return (
     <>
     <header >
-     <NavBar />
+     <NavBar/>
+     <TitleContainer title="Welcome to GentleMan"/>
     </header>
-   
+
     </>
   );
 }
---
+++ src/components/TitleContainer/TitleContainer.js
@@ -0,0 +1,11 @@
+
+const TitleContainer=(props)=>{
+    return(
+        <>
+        <div className="container">
+            <h1>{props.title}</h1>
+        </div>
+        </>
+    )
+}
+export default TitleContainer
