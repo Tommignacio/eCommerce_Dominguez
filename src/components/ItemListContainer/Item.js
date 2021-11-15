dc2ca33fa3c15a9397f1f97f07cf524bb8494dfa
Tommignacio <88063640+Tommignacio@users.noreply.github.com>
2021-11-15 02:12:06 -0300

    modificacion img

--- src/components/ItemListContainer/Item.js
+++ src/components/ItemListContainer/Item.js
@@ -3,9 +3,9 @@ const Item =(props)=>{
 
 return(
     <>
-        <div>
-            <p>{props.el.id}</p>
+        <div className="container">
             <p> {props.el.title}</p>
+            <img src={props.el.image}/>
             <p> {props.el.description}</p>
         </div>
     </>
--- src/components/ProductsApi/ProductsApi.js
+++ src/components/ProductsApi/ProductsApi.js
@@ -1,13 +1,15 @@
 const ProductsApi=[
     {
         id:1,
-        title:"Camisa negra",
+        image:"https://cdn.shopify.com/s/files/1/0246/9196/6018/products/CASEYSHIRTLBTB52019_3_800x.jpg?v=1630158918",
+        title:"Camisa blanca",
         price: 3500,
         description:"Camisa talles S, M, XL"
     }, 
     {
         id:2,
-        title:"Camisa blanca",
+        image:"https://cdn.shopify.com/s/files/1/0246/9196/6018/products/CAMISALONDONBKTB50009_3_aca2f217-3a7c-4164-932e-c3e3ebbbecfb_800x.jpg?v=1620236890",
+        title:"Camisa negra",
         price: 3500,
         description:"Camisa talles S, M, XL"
     }
